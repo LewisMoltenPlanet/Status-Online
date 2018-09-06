6493fd9a07bc40e6cef48f8e56f441fc5a058dcd
LewisMoltenPlanet <lewishawk92@gmail.com>
2018-09-06 13:29:44 +0100

    Added Proto Assets
    Added player script, vehicle, auto rifle, player model.

--- StatusOnline/Assets/Scripts/PlayerLook.cs
+++ StatusOnline/Assets/Scripts/PlayerLook.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void LockCursor() //Locking cursor.
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void CameraRotation() //Calculating and applying rotation. / Clamping max-min rotation.
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f) //Checking and applying clamp.
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+
+            ClampXAxisRotationToValue(270.0f); //Value "(270.0f)" = X axis rotation clamp. 
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+
+            ClampXAxisRotationToValue(90.0f); //Value "(90.0f)" = Y axis rotation clamp. 
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
 }
--- StatusOnline/Assets/Scripts/PlayerMove.cs
+++ StatusOnline/Assets/Scripts/PlayerMove.cs
@@ -4,13 +4,63 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Fields")]
+    [SerializeField] private string horizontalInputName, verticalInputName;
+    [SerializeField] private float movementSpeed, jumpMultiplier;
+    [SerializeField] private AnimationCurve jumpFalloff;
+    [SerializeField] private KeyCode jumpKey;
+
+    private CharacterController charController;
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement() //Calculates and initiates player movement.
+    {
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput(); 
+    }
+
+    private void JumpInput() //Calculates and initiates jumping.
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFalloff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
 }
