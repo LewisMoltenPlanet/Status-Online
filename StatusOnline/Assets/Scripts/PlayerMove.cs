11d795c579f0e1734c8f69339557c776d6f88e92
LewisMoltenPlanet <lewishawk92@gmail.com>
2018-09-06 14:42:31 +0100

    Improved player movement
    Double jump, sprint, fixed bug.

--- StatusOnline/Assets/Scripts/PlayerMove.cs
+++ StatusOnline/Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
     [Header("Fields")]
     [SerializeField] private string horizontalInputName, verticalInputName;
-    [SerializeField] private float movementSpeed, jumpMultiplier;
-    [SerializeField] private AnimationCurve jumpFalloff;
-    [SerializeField] private KeyCode jumpKey;
+    [SerializeField] private float movementSpeed, sprintSpeed, jumpMultiplier, doubleJumpMultiplier, jumpCoolDown;
+    [SerializeField] private KeyCode jumpKey, sprintKey;
+    [SerializeField] private AnimationCurve jumpFalloff, doubleJumpFalloff;
 
     private CharacterController charController;
 
-    private bool isJumping;
+    public bool isJumping;
+    public bool hasDoubleJumped;
 
     private void Awake()
     {
@@ -26,23 +26,48 @@ public class PlayerMove : MonoBehaviour {
 
     private void PlayerMovement() //Calculates and initiates player movement.
     {
-        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
-        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName);
+        float horizInput = Input.GetAxis(horizontalInputName);
 
         Vector3 forwardMovement = transform.forward * vertInput;
         Vector3 rightMovement = transform.right * horizInput;
 
-        charController.SimpleMove(forwardMovement + rightMovement);
+        if (Input.GetKey(sprintKey))
+        {
+            charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * sprintSpeed);
+        }
+        else
+        {
+            charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+        }
 
         JumpInput(); 
     }
 
     private void JumpInput() //Calculates and initiates jumping.
     {
-        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        jumpCoolDown -= Time.deltaTime;
+
+        if (!charController.isGrounded)
         {
             isJumping = true;
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            jumpCoolDown = 0.02f;
+            isJumping = true;
             StartCoroutine(JumpEvent());
+        }
+
+        if (Input.GetKeyDown(jumpKey) && isJumping && !hasDoubleJumped && jumpCoolDown <= 0.0f)
+        {
+            hasDoubleJumped = true;
+            StartCoroutine(DoubleJumpEvent());
         }
     }
 
@@ -54,7 +79,9 @@ public class PlayerMove : MonoBehaviour {
         do
         {
             float jumpForce = jumpFalloff.Evaluate(timeInAir);
+
             charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+
             timeInAir += Time.deltaTime;
 
             yield return null;
@@ -62,5 +89,25 @@ public class PlayerMove : MonoBehaviour {
 
         charController.slopeLimit = 45.0f;
         isJumping = false;
+    }
+
+    private IEnumerator DoubleJumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float doubleJumpForce = doubleJumpFalloff.Evaluate(timeInAir);
+
+            charController.Move(Vector3.up * doubleJumpForce * doubleJumpMultiplier * Time.deltaTime);
+
+            timeInAir += Time.deltaTime;
+
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        hasDoubleJumped = false;
     }
 }
